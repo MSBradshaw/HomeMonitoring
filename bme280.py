e58df17812587f825914ce357ae43b6cf411cb6e
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2020-09-03 22:25:06 -0600

    created python script to track weather data and a bash script to run it every 15 minutes

--- bme280.py
+++ bme280.py
@@ -1,9 +1,22 @@
 import board
 import busio
 import adafruit_bme280
-i2c = busio.I2C(board.SCL, board.SDA)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
+import datetime
+import time
 
-print("\nTemperature: %0.1f C" % bme280.temperature)
-print("Humidity: %0.1f %%" % bme280.humidity)
-print("Pressure: %0.1f hPa" % bme280.pressure)
+i2c = busio.I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x77)
+
+date = str(datetime.date.today())
+current_time = str(datetime.datetime.now().time())
+unix_time = str(time.time())
+temp = str(bme280.temperature)
+hum = str(bme280.humidity)
+pressure = str(bme280.pressure)
+
+line = date + '\t' + current_time + '\t' + str(unix_time) + '\t' + temp + '\t' + hum + '\t' + pressure + '\n'
+with open('/home/pi/all_data.tsv','a') as file:
+	file.write(line)
+
+with open('/home/pi/most_recent_data.tsv','w') as file:
+	file.write(line)
---
+++ collected_data.sh
@@ -0,0 +1,2 @@
+# run the script every 15 mintes (900 seconds)
+watch -n 900 python3 bme280.py
