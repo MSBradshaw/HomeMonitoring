9d297b74f5a26e7191b35cc4902bffe8a076fdc9
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2020-09-07 12:31:43 -0600

    added command line param to collect_data.sh

--- collected_data.sh
+++ collected_data.sh
@@ -1,2 +1,3 @@
+# one command line argument, name that the data should be labeled with: basement, upstairs or main
 # run the script every 15 mintes (900 seconds)
-watch -n 900 python3 bme280.py
+watch -n 900 python3 bme280.py $1
