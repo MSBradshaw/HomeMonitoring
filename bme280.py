b2b5f6a51202c764391ccd01efbad2ec1ce07f89
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2020-09-07 12:25:22 -0600

    created script for collected data from workers

--- bme280.py
+++ bme280.py
@@ -3,6 +3,7 @@ import busio
 import adafruit_bme280
 import datetime
 import time
+import sys
 
 i2c = busio.I2C(board.SCL, board.SDA)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x77)
---
+++ harvest.sh
@@ -0,0 +1,2 @@
+scp pi@10.0.0.2:~/most_recent_data.tsv ./most_recent_1.tsv
+scp pi@10.0.0.34:~/most_recent_data.tsv ./most_recent_2.tsv
